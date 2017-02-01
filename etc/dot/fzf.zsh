6b4882c6bd7e4e9b5f0d44e34de1e921f490f962
Sebastian Thiel <sebastian.thiel@thoughtworks.com>
2017-02-01 17:03:37 +0100

    fzf support

---
+++ etc/dot/fzf.zsh
@@ -0,0 +1,10 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */Users/sthiel/.fzf/bin* ]]; then
+  export PATH="$PATH:/Users/sthiel/.fzf/bin"
+fi
+
+# Key bindings
+# ------------
+source "/Users/sthiel/.fzf/shell/key-bindings.zsh"
+
