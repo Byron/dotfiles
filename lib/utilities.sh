446f1b6bf206351df3d631ef21aa7972ccbd7e43
Sebastian Thiel <byronimo@gmail.com>
2017-02-02 07:29:53 +0100

    Auto-install alacritty

--- lib/utilities.sh
+++ lib/utilities.sh
@@ -14,12 +14,16 @@ function clone_or_pull () {
 
 function update_everything () {
     local brew_owner="$(/usr/bin/stat -f %Su "$(command -v brew)")"
+    local npm_owner="$(/usr/bin/stat -f %Su "$(command -v npm)")"
+    
     yes | apm update &
     (sudo -u $brew_owner -i brew update && sudo -u $brew_owner -i brew upgrade; sudo -u $brew_owner -i brew cleanup) &
-    (npm install npm -g && npm update -g) &
-    rustup update stable &
-    cargo install-update --all &
-    cargo install --force --git https://github.com/jwilm/alacritty &
+    (sudo -u $npm_owner -i npm install npm -g && sudo -u $npm_owner -i npm update -g) &
+    (rustup update stable && (
+      cargo +nightly install-update --all &
+      cargo +nightly install --force --git https://github.com/jwilm/alacritty &
+      wait
+    )) &
 
     wait
 }
