ecf69d62479890066139646932559276f5916a82
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-01-29 10:02:40 +0800

    For now, only link dotfiles during installation
    Everything else really needs a little updating. Probably not
    too much work though.

--- bin/install.sh
+++ bin/install.sh
@@ -8,22 +8,8 @@ for lib in utilities install; do
   source $our_dir/../lib/$lib.sh
 done
 
-echo CLONING REPOSITORIES
-clone_repositories
-
-echo BREWING PROGRAMS
-setup_brew
-
-echo SETTING UP RUST
-setup_rust
-
-echo SETUP USER
-setup_user
-
 echo LINKING DOTFILES
 link_dotfiles "$(cd $our_dir/.. && pwd)" etc
 
-echo APPLYING OSX SETTINGS
-setup_osx
 
 echo DONE
