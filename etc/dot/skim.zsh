45e068e576896a8474a66a4ff6668107ff168f05
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-12-06 22:49:29 +0800

    cleanup and fzf replaced with skim

---
+++ etc/dot/skim.zsh
@@ -0,0 +1,8 @@
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/Users/byron/dev/skim/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/Users/byron/dev/skim/shell/key-bindings.zsh"
