f5599a5149bbb9b1463ab064c967436c3e5eaaad
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-01-24 14:14:39 +0800

    More general implementation of searching git repositories

--- etc/dot/skim.zsh
+++ etc/dot/skim.zsh
@@ -1,8 +1,8 @@
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/byron/dev/skim/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/Users/byron/dev/github.com/lotabout/skim/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/byron/dev/skim/shell/key-bindings.zsh"
+source "/Users/byron/dev/github.com/lotabout/skim/shell/key-bindings.zsh"
