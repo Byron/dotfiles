7b3f5f1dc5cab482c4ca1d4744226ff35909b1a5
Sebastian Thiel <sebastian.thiel@icloud.com>
2026-04-28 10:38:21 +0800

    allow Ctrl XE in fish

--- etc/config/fish/functions/fish_user_key_bindings.fish
+++ etc/config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,4 @@
 function fish_user_key_bindings --description 'User key bindings'
     bind ctrl-e 'if commandline --search-mode; or commandline --paging-mode; commandline -f execute; else; commandline -f end-of-line; end'
+    bind ctrl-x,ctrl-e edit_command_buffer
 end
